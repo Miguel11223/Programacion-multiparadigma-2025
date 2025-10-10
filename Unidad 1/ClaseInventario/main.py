c06992922ea23c889aece35d593f3b5fb91634b0
MiguelDav <terominecraft5@gmail.com>
2025-10-10 11:20:07 -0500

    Comparacion

--- Unidad 1/ClaseInventario/main.py
+++ Unidad 1/ClaseInventario/main.py
@@ -18,3 +18,19 @@ def main():
     inventario.agregar_producto(mouse)
     inventario.agregar_producto(teclado)
     inventario.agregar_producto(monitor)
+
+    print("\nProductos disponibles para comparar:")
+    print(", ".join([producto.nombre for producto in inventario._Inventario__productos.values()]))
+    nombre1 = input("Ingrese el nombre del primer producto a comparar: ")
+    nombre2 = input("Ingrese el nombre del segundo producto a comparar: ")
+
+    producto1 = inventario.buscar_producto(nombre1)
+    producto2 = inventario.buscar_producto(nombre2)
+
+    if producto1 and producto2:
+        print(f"\n¿Son iguales los productos {nombre1} y {nombre2}? {producto1 == producto2}")
+    else:
+        print("\nUno o ambos productos no se encontraron en el inventario.")
+
+        
+
