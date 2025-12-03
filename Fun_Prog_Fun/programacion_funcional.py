7fd05be07b3d9c74bcc9b529c2146d83401ca5cc
MiguelDav <terominecraft5@gmail.com>
2025-12-03 10:25:18 -0600

    Funcion B y C

---
+++ Fun_Prog_Fun/programacion_funcional.py
@@ -0,0 +1,26 @@
+# Función B
+contador = 0
+def siguiente_id():
+    global contador
+    contador += 1
+    return f"ID-{contador}"
+
+
+#Funcion B correccion pura
+def siguiente_id_pura(contador_actual):
+    nuevo_contador = contador_actual + 1
+    nuevo_id = f"ID-{nuevo_contador}"
+    return nuevo_id, nuevo_contador  
+
+# Función C
+def agregar_fecha(registro):
+    from datetime import datetime
+    registro['fecha'] = datetime.now().isoformat()
+    return registro
+
+#Funcion C correccion pura
+def agregar_fecha_pura(registro, fecha_actual):
+    registro_copia = registro.copy()
+    registro_copia['fecha'] = fecha_actual
+    return registro_copia
+
