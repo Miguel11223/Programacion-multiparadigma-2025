8637493db01fe6b6f395d600e811e165e769580b
MiguelDav <terominecraft5@gmail.com>
2025-12-03 18:56:51 -0600

    Funcion 3 y 4

--- Fun_Prog_Fun_Part_3_4/asignacion_01.py
+++ Fun_Prog_Fun_Part_3_4/asignacion_01.py
@@ -11,3 +11,15 @@ def crear_filtro(predicado):
         return list(filter(predicado, lista))
     return filtro
 
+def crear_reductor(funcion, valor_inicial):
+    def reductor(lista):
+       return reduce(funcion, lista, valor_inicial)
+    return reductor
+
+def componer(*funciones):
+    def procesador_compuesto(dato):
+        resultado = dato
+        for func in funciones:
+            resultado = func(resultado)
+        return resultado
+    return procesador_compuesto
